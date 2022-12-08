cacf8c39df7373d7e81d7741f53be954ec9f9aa5
BelkhirSaber <saberzw.belkhir@gmail.com>
2022-12-08 21:04:03 +0100

    end admin dashboard home page

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,8 @@ import "boxicons";
 import Chart from "chart.js/auto";
 // Sales Chart
 const ctx = document.getElementById("salesChart");
+Chart.defaults.borderColor = "#EBEDF0";
+// Chart.defaults.borderWidth = "1px";
 const myChart = new Chart(ctx, {
   type: "line",
   data: {
@@ -19,17 +21,66 @@ const myChart = new Chart(ctx, {
         label: "Sales Last 7 days",
         data: [102, 190, 172, 159, 300, 95, 232, 309],
         tension: 0.4,
-        backgroundColor: "hsl(210, 82%, 57%)",
+        // backgroundColor: "hsl(210, 82%, 57%)",
         borderColor: "hsl(210, 82%, 57%)",
+        backgroundColor: "hsla(210, 82%, 57%, .2)",
+        borderWidth: 1,
+        fill: true,
+        pointStyle: "solid",
       },
+
+      // {
+      //   label: "time Last 7 days",
+      //   data: [3, 90, 272, 359, 120, 95, 299, 509],
+      //   tension: 0.4,
+      //   // backgroundColor: "hsl(210, 82%, 57%)",
+      //   borderColor: "hsl(0, 0%, 69%)",
+      //   backgroundColor: "hsla(0, 0%, 69%, .2)",
+      //   borderWidth: 1,
+      //   fill: true,
+      // },
     ],
   },
   options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+        align: "end",
+
+        labels: {
+          usePointStyle: false,
+          pointStyle: "circle",
+        },
+      },
+      // title: {
+      //   display: true,
+      //   text: "Last 7 Days",
+      //   color: "#000",
+      //   position: "top",
+      //   align: "start",
+      //   font: {
+      //     size: "15rem",
+      //     weight: "normal",
+      //     family: "'Roboto', sans-serif",
+      //   },
+      // },
+    },
     scales: {
       y: {
         beginAtZero: true,
+        // suggestedMax: 1000,
+        ticks: {
+          callback: function (value, index, ticks) {
+            // return "$ " + value;
+            // return ticks.index ;
+
+            return "$ " + value;
+          },
+        },
       },
-      grid: {
+
+      x: {
         display: false,
       },
     },
